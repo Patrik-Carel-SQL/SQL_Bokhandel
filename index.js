0fb0f9ebe0dea519669d2efe5d94519b10e3bd84
Carel Williams <carel@live.se>
2021-04-30 11:04:51 +0200

    Adds a css file with styling for the td in the table. Adjusted the pug file aswell

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const express = require("express")
-const sql = require("mssql")
+const express = require('express');
+const sql = require('mssql');
 
-require("dotenv").config()
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
 app.set('view engine', 'pug');
 app.locals.pretty = true;
 
-
-
 app.get('/', async (req, res) => {
-
-  try {
-  // Setup select from SQL server
-    const query=`
+	try {
+		// Setup select from SQL server
+		const query = `
       select
         ISBN13 as 'ISBN',
         Titel,
@@ -23,22 +20,18 @@ app.get('/', async (req, res) => {
       from
         Böcker as B
       join Författare as FF on B.FörfattareID = FF.ID
-    `
-    
-    const connection = await sql.connect(process.env.CONNECTION)
-    const result = await connection.request()
-      .query(query)
-    console.log(query)
-    
-    res.render('bocker.pug', {bocker: result.recordset})
-  }
+    `;
 
-  catch (ex){
-    console.log(ex)
-  }
-})
+		const connection = await sql.connect(process.env.CONNECTION);
+		const result = await connection.request().query(query);
+		console.log(query);
 
+		res.render('bocker.pug', { bocker: result.recordset });
+	} catch (ex) {
+		console.log(ex);
+	}
+});
 
 app.listen(3000, () => {
-  console.log("Server is now online with port 3000.")
-})
+	console.log('Server is now online with port 3000.');
+});
